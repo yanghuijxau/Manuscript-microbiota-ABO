ddc0d4dd0be210170c6146b6b00b1944c6b75144
yanghuijxau <389846652@qq.com>
2022-03-11 09:44:35 +0800

    Add files via upload

---
+++ source_data/Figures/Fig2/Fig2D/Fig2D.r
@@ -0,0 +1,6 @@
+##家系间差异
+rm(list=ls())
+library(pheatmap)
+ taxa<-read.table("data.txt",sep="\t",header=T,row.names=1)
+
+pheatmap(taxa,scale="none",cluster_rows=F,cluster_cols=F,clustering_method="average")
