d14592ad0843307cd9e65c45b72d32f8a5c1994a
yanghuijxau <389846652@qq.com>
2022-03-11 09:41:41 +0800

    Add files via upload

---
+++ source_data/Figures/Fig2/Fig2A/Fig2A.r
@@ -0,0 +1,9 @@
+
+data <- read.csv("kinship_dissimilarity_within_litter.txt", header=T, sep="")
+
+
+library(ggplot2)
+
+data$group <- factor(data$group,levels = c("F6_D25","F7_D25","F6_D120","F7_D120","F6_D240","F7_D240","F6_IC","F7_IC","F6_CC","F7_CC","F7_IM","F7_CM"))
+
+ggplot(data,aes(x=kinship,y=microbiome,color=group2))+geom_point(shape=1,fill=NA,size=1.2)+stat_smooth(method = lm,se=F,color="#696969",size=0.5)+theme_bw()+xlab("Genetic kinship")+ylab("Microbiome dissimilarity")+theme(axis.text = element_text(size=9),axis.title = element_text(size=12),axis.line = element_line(color="black"),panel.grid = element_blank())+scale_color_manual(values = c("#3B4992","#BE703B","#EE0000","#008B45","#631879","#0099B4","#FF00C0"))+facet_wrap(~group,nrow = 2)
